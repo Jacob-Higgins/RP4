168c581e4d539072ecab0c22bea8a47407be1bf1
Jacob-Higgins <140733772+Jacob-Higgins@users.noreply.github.com>
2025-03-02 14:36:11 +0000

    Removal of all mentions of Severity and FMECAs, readjustment to focus on just plume model and impingement model and to assess risk from that. Base from which to expand methodology and introduction. Seems to be considerable overlap between introduction and methodolgy at the moment. No results or further written yet

--- TEMP.py
+++ TEMP.py
@@ -1,15 +1,3 @@
-import serial
-import time
+import _cea as cea
 
-# Replace 'COM1' with the correct port name, e.g., '/dev/ttyUSB0' on Linux
-ser = serial.Serial('COM3', 9600, timeout=1)
-
-print("Listening on serial port...")
-time.sleep(2)
-command = "C2\n"
-ser.write(command.encode('utf-8'))
-while True:
-
-    data = ser.read(1)  # Read one byte at a time
-    if data:
-        print(data.hex())
+print("hello")
